0aab548f4e54a5b9727ac0291beea6849ad768ad
ismailylmz31 <64439554+ismailylmz31@users.noreply.github.com>
2024-11-28 16:54:08 +0300

    Event service updated

--- src/projects/techCareerProject/TechCareer.Service/Abstracts/IEventService.cs
+++ src/projects/techCareerProject/TechCareer.Service/Abstracts/IEventService.cs
@@ -1,20 +1,44 @@
-﻿using System;
+﻿using Core.Persistence.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using TechCareer.Models.Dtos.Roles;
+using TechCareer.Models.Entities;
 using TechCareer.Models.Events;
 
 namespace TechCareer.Service.Abstracts
 {
     public interface IEventService
     {
+        // Sade Add, Update, Delete metotları
         Task<EventResponseDto> AddAsync(CreateEventRequestDto dto);
-        Task<string> DeleteAsync(Guid id);
-        Task<List<EventResponseDto>> GetAllAsync();
-        Task<EventResponseDto> GetByIdAsync(Guid id);
-        Task<EventResponseDto> updateAsync(Guid id, UpdateEventRequestDto dto);
-    }
+        Task<string> DeleteAsync(Guid id, bool permanent = false);
+        Task<EventResponseDto> UpdateAsync(Guid id, UpdateEventRequestDto dto);
 
+        // Listeleme ve gösterim metotları
+        Task<List<EventResponseDto>> GetListAsync(
+            Expression<Func<Event, bool>>? predicate = null,
+            Func<IQueryable<Event>, IOrderedQueryable<Event>>? orderBy = null,
+            bool include = false,
+            bool withDeleted = false,
+            bool enableTracking = true,
+            CancellationToken cancellationToken = default
+        );
+
+
+        Task<Paginate<EventResponseDto>> GetPaginateAsync(
+            Expression<Func<Event, bool>>? predicate = null,
+            Func<IQueryable<Event>, IOrderedQueryable<Event>>? orderBy = null,
+            bool include = true,
+            int index = 0,
+            int size = 10,
+            bool withDeleted = false,
+            bool enableTracking = true,
+            CancellationToken cancellationToken = default
+        );
+
+        Task<EventResponseDto> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    }
 }
--- src/projects/techCareerProject/TechCareer.Service/Concretes/EventService.cs
+++ src/projects/techCareerProject/TechCareer.Service/Concretes/EventService.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
 using Core.AOP.Aspects;
+using Core.Persistence.Extensions;
+using System.Linq.Expressions;
 using TechCareer.DataAccess.Repositories.Abstracts;
+using TechCareer.Models.Dtos.Roles;
 using TechCareer.Models.Entities;
 using TechCareer.Models.Events;
 using TechCareer.Service.Abstracts;
@@ -9,63 +12,109 @@ using TechCareer.Service.Rules;
 
 namespace TechCareer.Service.Concretes
 {
-    public class EventService(
-        IEventRepository eventRepository,
-        IMapper mapper,
-        EventBusinessRules businessRules
-    ) : IEventService
+    public class EventService : IEventService
     {
+        private readonly IEventRepository _eventRepository;
+        private readonly IMapper _mapper;
+        private readonly EventBusinessRules _businessRules;
+
+        public EventService(IEventRepository eventRepository, IMapper mapper, EventBusinessRules businessRules)
+        {
+            _eventRepository = eventRepository;
+            _mapper = mapper;
+            _businessRules = businessRules;
+        }
+
         [LoggerAspect]
         [ClearCacheAspect("Events")]
         [AuthorizeAspect("Admin")]
         public async Task<EventResponseDto> AddAsync(CreateEventRequestDto dto)
         {
-            await businessRules.EventTitleMustBeUnique(dto.Title);
+            await _businessRules.EventTitleMustBeUnique(dto.Title);
 
-            var eventEntity = mapper.Map<Event>(dto);
+            var eventEntity = _mapper.Map<Event>(dto);
             eventEntity.Id = Guid.NewGuid();
 
-            var addedEvent = await eventRepository.AddAsync(eventEntity);
-
-            return mapper.Map<EventResponseDto>(addedEvent);   
+            var addedEvent = await _eventRepository.AddAsync(eventEntity);
+            return _mapper.Map<EventResponseDto>(addedEvent);
         }
 
         [LoggerAspect]
         [ClearCacheAspect("Events")]
         [AuthorizeAspect("Admin")]
-        public async Task<string> DeleteAsync(Guid id)
+        public async Task<string> DeleteAsync(Guid id, bool permanent = false)
         {
-            var eventEntity = await businessRules.EventMustExist(id);   
-            await eventRepository.DeleteAsync(eventEntity);
+            var eventEntity = await _businessRules.EventMustExist(id);
+            await _eventRepository.DeleteAsync(eventEntity, permanent);
             return EventMessages.EventDeleted;
         }
 
-        [CacheAspect(cacheKeyTemplate: "EventList", bypassCache: false, cacheGroupKey: "Events")]
-        public async Task<List<EventResponseDto>> GetAllAsync()
-        {
-            var events = await eventRepository.GetListAsync(include: true, enableTracking: false);
-            return mapper.Map<List<EventResponseDto>>(events);
-        }
-
-        public async Task<EventResponseDto> GetByIdAsync(Guid id)
-        {
-            var eventEntity = await businessRules.EventMustExist(id);
-            return mapper.Map<EventResponseDto>(eventEntity);
-        }
-
         [LoggerAspect]
         [ClearCacheAspect("Events")]
         [AuthorizeAspect("Admin")]
-        public async Task<EventResponseDto> updateAsync(Guid id, UpdateEventRequestDto dto)
+        public async Task<EventResponseDto> UpdateAsync(Guid id, UpdateEventRequestDto dto)
         {
-            var eventEntity = await businessRules.EventMustExist(id);
+            var eventEntity = await _businessRules.EventMustExist(id);
 
-            mapper.Map(dto, eventEntity);
+            _mapper.Map(dto, eventEntity);
 
-            var updatedEvent = await eventRepository.UpdateAsync(eventEntity);
-            return mapper.Map<EventResponseDto>(updatedEvent);
+            var updatedEvent = await _eventRepository.UpdateAsync(eventEntity);
+            return _mapper.Map<EventResponseDto>(updatedEvent);
         }
 
-        
+        [CacheAspect(cacheKeyTemplate: "EventList", bypassCache: false, cacheGroupKey: "Events")]
+        public async Task<List<EventResponseDto>> GetListAsync(
+            Expression<Func<Event, bool>>? predicate = null,
+            Func<IQueryable<Event>, IOrderedQueryable<Event>>? orderBy = null,
+            bool include = false,
+            bool withDeleted = false,
+            bool enableTracking = true,
+            CancellationToken cancellationToken = default)
+        {
+            var events = await _eventRepository.GetListAsync(predicate, orderBy, include, withDeleted, enableTracking, cancellationToken);
+            return _mapper.Map<List<EventResponseDto>>(events);
+        }
+
+
+        public async Task<Paginate<EventResponseDto>> GetPaginateAsync(
+            Expression<Func<Event, bool>>? predicate = null,
+            Func<IQueryable<Event>, IOrderedQueryable<Event>>? orderBy = null,
+            bool include = true,
+            int index = 0,
+            int size = 10,
+            bool withDeleted = false,
+            bool enableTracking = true,
+            CancellationToken cancellationToken = default)
+        {
+            // Repository'den Paginate<Event> verisini alıyoruz
+            var events = await _eventRepository.GetPaginateAsync(
+                predicate,
+                orderBy,
+                include,
+                index,
+                size,
+                withDeleted,
+                enableTracking,
+                cancellationToken
+            );
+
+            // Paginate<EventResponseDto> nesnesi oluşturuyoruz
+            return new Paginate<EventResponseDto>
+            {
+                Items = _mapper.Map<IList<EventResponseDto>>(events.Items),
+                Index = events.Index,
+                Size = events.Size,
+                Count = events.Count,
+                Pages = events.Pages
+            };
+        }
+
+
+
+        public async Task<EventResponseDto> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            var eventEntity = await _businessRules.EventMustExist(id);
+            return _mapper.Map<EventResponseDto>(eventEntity);
+        }
     }
 }
