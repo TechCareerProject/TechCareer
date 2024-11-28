fac28344944cca23329323535ffae06482d2c61b
elifksc <eekuscu14@gmail.com>
2024-11-28 11:12:47 +0300

    videoeducation entities created

--- src/projects/techCareerProject/TechCareer.Models/Entities/videoEducation.cs
+++ src/projects/techCareerProject/TechCareer.Models/Entities/videoEducation.cs
@@ -1,7 +1,18 @@
 ﻿
 
+using Core.Persistence.Repositories.Entities;
+using System.Reflection.Metadata;
+
 namespace TechCareer.Models.Entities;
 
-public class videoEducation
+public class videoEducation : Entity<int>
 {
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public double TotalHour { get; set; }
+    public bool IsCertified { get; set; }
+    public Level Level { get; set; }
+    public string ImageUrl { get; set; }
+    public Guid InstrutorId { get; set; }
+    public string ProgrammingLanguage { get; set; }
 }
