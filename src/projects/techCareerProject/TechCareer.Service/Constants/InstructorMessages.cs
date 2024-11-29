d0aa635ee65ea1555f1c8b31df1287c657e9fdaa
Muhammet KUS <muhametkus@gmail.com>
2024-11-29 23:51:00 +0300

    Create InstructorMessages.cs

---
+++ src/projects/techCareerProject/TechCareer.Service/Constants/InstructorMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace TechCareer.Service.Constants;
+
+public static class InstructorMessages
+{
+    public const string InstructorNotFound = "The instructor could not be found.";
+    public const string EmailAlreadyExists = "The email is already associated with another instructor.";
+}
