131bce21c00656573a047ed43ad34f6dda3e21e3
Muhammet KUS <muhametkus@gmail.com>
2024-11-29 23:50:52 +0300

    Create InstructorBusinessRules.cs

---
+++ src/projects/techCareerProject/TechCareer.Service/Rules/InstructorBusinessRules.cs
@@ -0,0 +1,39 @@
+﻿using Core.CrossCuttingConcerns.Exceptions.ExceptionTypes;
+using Core.CrossCuttingConcerns.Rules;
+using Core.Security.Entities;
+using TechCareer.DataAccess.Repositories.Abstracts;
+using TechCareer.Service.Constants;
+using TechCareer.Service.Models;
+
+namespace TechCareer.Service.Rules;
+
+public sealed class InstructorBusinessRules(IInstructorRepository _instructorRepository) : BaseBusinessRules
+{
+    public Task InstructorShouldExistWhenSelected(Instructor? instructor)
+    {
+        if (instructor == null)
+            throw new BusinessException(InstructorMessages.InstructorNotFound);
+        return Task.CompletedTask;
+    }
+
+    public async Task InstructorIdShouldExistWhenSelected(Guid id)
+    {
+        bool doesExist = await _instructorRepository.AnyAsync(predicate: i => i.Id == id, enableTracking: false);
+        if (!doesExist)
+            throw new BusinessException(InstructorMessages.InstructorNotFound);
+    }
+
+    public async Task InstructorEmailShouldNotExistWhenInsert(string email)
+    {
+        bool doesExist = await _instructorRepository.AnyAsync(predicate: i => i.Email == email, enableTracking: false);
+        if (doesExist)
+            throw new BusinessException(InstructorMessages.EmailAlreadyExists);
+    }
+
+    public async Task InstructorEmailShouldNotExistWhenUpdate(Guid id, string email)
+    {
+        bool doesExist = await _instructorRepository.AnyAsync(predicate: i => i.Id != id && i.Email == email, enableTracking: false);
+        if (doesExist)
+            throw new BusinessException(InstructorMessages.EmailAlreadyExists);
+    }
+}
